964f8a5f7992f013db1039043899d7db7eb3feab
Robert Thurnher <r.thurnher@gmail.com>
2014-02-16 21:18:30 +0100

    Simplify things

--- src/main/java/com/github/robi42/boot/ApplicationInitializer.java
+++ src/main/java/com/github/robi42/boot/ApplicationInitializer.java
@@ -2,13 +2,10 @@ package com.github.robi42.boot;
 
 import com.github.robi42.boot.rest.JerseyConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
-import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -20,14 +17,6 @@ import static org.glassfish.jersey.servlet.ServletProperties.JAXRS_APPLICATION_C
 @Configuration
 @EnableAutoConfiguration
 public class ApplicationInitializer extends SpringBootServletInitializer {
-
-    @Value("${server.port:8080}")
-    private int serverPort;
-
-    @Bean
-    public EmbeddedServletContainerFactory jettyServletContainerFactory() {
-        return new JettyEmbeddedServletContainerFactory(serverPort);
-    }
 
     @Bean
     public ServletRegistrationBean jerseyServlet() {
