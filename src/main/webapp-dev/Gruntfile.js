29efec2a91e2aeefd14665caf7f96537872cf73b
Robert Thurnher <r.thurnher@gmail.com>
2014-02-21 21:48:21 +0100

    Upgrade to latest Jetty

--- src/main/webapp-dev/Gruntfile.js
+++ src/main/webapp-dev/Gruntfile.js
@@ -255,7 +255,6 @@ module.exports = function (grunt) {
             '.htaccess',
             '*.html',
             'views/{,*/}*.html',
-            'bower_components/**/*',
             'images/{,*/}*.{webp}',
             'fonts/*'
           ]
