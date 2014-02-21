5cd019844648578ec90467a90b6be43608346207
Robert Thurnher <r.thurnher@gmail.com>
2014-02-21 18:05:43 +0100

    Wording

--- src/main/java/com/github/robi42/boot/SecurityConfig.java
+++ src/main/java/com/github/robi42/boot/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                // App paths
+                // SPA paths
                 .antMatchers("/", "/views/**", "/scripts/**", "/styles/**", "/images/**").permitAll()
                 // API endpoints
                 .antMatchers("/api/**").permitAll()
