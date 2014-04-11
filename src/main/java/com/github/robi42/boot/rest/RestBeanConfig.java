20fc15f7fce45708f2299e599b256ae81b046931
Robert Thurnher <r.thurnher@gmail.com>
2014-04-11 20:39:05 +0200

    Minor

--- src/main/java/com/github/robi42/boot/rest/RestBeanConfig.java
+++ src/main/java/com/github/robi42/boot/rest/RestBeanConfig.java
@@ -3,9 +3,11 @@ package com.github.robi42.boot.rest;
 import com.github.robi42.boot.dao.MessageRepository;
 import com.github.robi42.boot.domain.util.BeanValidator;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import javax.inject.Inject;
 
+@Configuration
 public class RestBeanConfig {
     @Inject
     private MessageRepository messageRepository;
