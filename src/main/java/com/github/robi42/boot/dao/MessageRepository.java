0ef83f17b9e0e2e5bc0faea651b5b8c8663302e1
Robert Thurnher <r.thurnher@gmail.com>
2014-04-12 15:27:35 +0200

    Minor

--- src/main/java/com/github/robi42/boot/dao/MessageRepository.java
+++ src/main/java/com/github/robi42/boot/dao/MessageRepository.java
@@ -2,7 +2,5 @@ package com.github.robi42.boot.dao;
 
 import com.github.robi42.boot.domain.Message;
 import org.springframework.data.elasticsearch.repository.ElasticsearchCrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MessageRepository extends ElasticsearchCrudRepository<Message, String> {}
