cb68dd78299a90fad2116ef9e082e2421b6c80a2
Robert Thurnher <r.thurnher@gmail.com>
2014-04-15 23:32:36 +0200

    Refine testing setup

--- src/test/java/com/github/robi42/boot/util/IntegrationTestBase.java
+++ src/test/java/com/github/robi42/boot/util/IntegrationTestBase.java
@@ -1,51 +1,30 @@
 package com.github.robi42.boot.util;
 
 import com.github.robi42.boot.TestApplicationInitializer;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.ConfigurableEnvironment;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.SpringApplicationContextLoader;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.inject.Inject;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.WebTarget;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeoutException;
 
-import static java.util.concurrent.Executors.newSingleThreadExecutor;
-import static java.util.concurrent.TimeUnit.SECONDS;
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TestApplicationInitializer.class, loader = SpringApplicationContextLoader.class)
+@ActiveProfiles("test")
 public abstract class IntegrationTestBase {
+    @Value("${server.port}")
+    private int serverPort;
+    @Inject
+    private Client webClient;
     protected WebTarget restApi;
-
-    private static Client webClient;
-    private static int serverPort;
-    private static ConfigurableApplicationContext applicationContext;
-
-    @BeforeClass
-    public static void init() throws InterruptedException, ExecutionException, TimeoutException {
-        final Future<ConfigurableApplicationContext> applicationContextFuture = newSingleThreadExecutor()
-                .submit(() -> {
-                    System.setProperty("spring.profiles.active", "test");
-                    return SpringApplication.run(TestApplicationInitializer.class);
-                });
-        applicationContext = applicationContextFuture.get(60, SECONDS);
-        final ConfigurableEnvironment environment = applicationContext.getEnvironment();
-        serverPort = environment.getProperty("server.port", int.class);
-        webClient = applicationContext.getBean(Client.class);
-    }
 
     @Before
     public void setUp() {
         restApi = webClient.target(String.format("http://localhost:%d/api", serverPort));
-    }
-
-    @AfterClass
-    public static void cleanUp() {
-        if (applicationContext != null) {
-            applicationContext.close();
-        }
     }
 }
